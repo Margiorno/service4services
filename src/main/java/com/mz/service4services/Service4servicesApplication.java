bb56dab4b063b97682b808197b15f27f45b7770e
Margiorno <m.zawiski03@gmail.com>
2025-04-04 23:50:23 +0200

    project initialization

---
+++ src/main/java/com/mz/service4services/Service4servicesApplication.java
@@ -0,0 +1,13 @@
+package com.mz.service4services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service4servicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service4servicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mz/service4services/Service4servicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mz.service4services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service4servicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
