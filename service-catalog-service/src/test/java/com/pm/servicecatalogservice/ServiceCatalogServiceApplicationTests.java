714fee0e3b90cb56808decbf1d5aa9690308831e
Margiorno <m.zawiski03@gmail.com>
2025-07-05 13:28:27 +0200

    service catalog initialized

---
+++ service-catalog-service/src/test/java/com/pm/servicecatalogservice/ServiceCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.servicecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
